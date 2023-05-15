5c79689526136a9e422af0467fb01e73a4616318
Tashrik Anam <anampartho31@gmail.com>
2023-05-15 19:28:07 +0600

    Prevent username and password from being empty string

--- server/auth/index.js
+++ server/auth/index.js
@@ -11,13 +11,13 @@ const users = db.get("users");
 users.createIndex("username", { unique: true });
 
 const schema = Joi.object({
-  // TODO: Check for empty strings
   username: Joi.string()
+    .trim()
     .pattern(new RegExp("^[a-zA-Z0-9_]+$"))
     .min(3)
     .max(30)
     .required(),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  password: Joi.string().trim().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
 });
 
 router.get("/", (req, res) => {
