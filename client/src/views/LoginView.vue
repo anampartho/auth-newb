9942480e044710e5745dd7ddc64714faed65ec1b
Tashrik Anam <anampartho31@gmail.com>
2023-05-17 15:41:07 +0600

    Add login route

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="sign-up">
+    <div class="jumbotron jumbotron-fluid py-4 mt-4">
+      <h1 class="display-4">Login</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'LoginView',
+  methods: {}
+};
+</script>
