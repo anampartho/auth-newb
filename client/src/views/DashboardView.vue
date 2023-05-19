0521a376877da9f77b2055a72c004495b19b98ee
Tashrik Anam <anampartho31@gmail.com>
2023-05-19 19:23:56 +0600

    Show username and add logout functionality

--- client/src/views/DashboardView.vue
+++ client/src/views/DashboardView.vue
@@ -2,14 +2,54 @@
   <div class="dashboard">
     <div class="jumbotron jumbotron-fluid py-4 mt-4">
       <h1 class="display-4">Dashboard</h1>
+      <p class="lead" v-if="user.username">
+        Welcome <strong>{{ user?.username }} 👋</strong>
+      </p>
+      <p class="lead mt-4">
+        <a
+          class="btn btn-primary btn-lg"
+          role="button"
+          @click.prevent="logout"
+          @keydown.prevent="logout"
+          tabindex="0"
+          >Logout</a
+        >
+      </p>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+const API_URL = 'http://localhost:5000';
 
 export default {
-  name: 'DashboardView'
+  name: 'DashboardView',
+  data: () => ({
+    user: {}
+  }),
+  mounted() {
+    fetch(API_URL, {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log({ res });
+        if (res.user) {
+          this.user = res.user;
+        } else {
+          localStorage.removeItem('token');
+        }
+      });
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem('token');
+      this.$router.push('/login');
+    }
+  }
 };
 </script>
