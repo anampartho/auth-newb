b39fa022a7b192eea03d625f697d86366d9c9480
Tashrik Anam <anampartho31@gmail.com>
2023-05-19 18:12:58 +0600

    Add dashboard view

---
+++ client/src/views/DashboardView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="dashboard">
+    <div class="jumbotron jumbotron-fluid py-4 mt-4">
+      <h1 class="display-4">Dashboard</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'DashboardView'
+};
+</script>
