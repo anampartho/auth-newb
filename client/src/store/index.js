937cade2eda76b57ce7bf4a7364c920c18688260
Tashrik Anam <anampartho31@gmail.com>
2023-05-20 16:09:20 +0600

    Use vuex-plugin-persistedstate to persist vuex store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
+import createPersistedState from 'vuex-plugin-persistedstate';
 import authModule from './modules/auth';
 
 const store = createStore({
   modules: {
     auth: authModule
-  }
+  },
+  plugins: [createPersistedState()]
 });
 
 export default store;
